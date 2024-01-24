f13c529edc45f00c3092887d7b8709b124a3fc2a
Calvin Buckle <ccccbuckle@gmail.com>
2024-01-24 16:46:08 +0200

    align form results correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './index.css';
+import './styles.css';
 import { useState } from 'react';
 import { Education } from './components/Forms';
 import { Experience } from './components/Forms';
@@ -12,20 +12,96 @@ function App() {
 const [eduForm, setEduForm] = useState([])
 const [expForm, setExpForm] = useState([])
 
-const [result, setResult] = useState('');
+
+const [schoolResult, setSchoolResult] = useState('')
+const [degreeResult, setDegreeResult] = useState('')
+const [descriptionResult, setDescriptionResult] = useState('')
+const [startDateResult, setStartDateResult] = useState('')
+const [endDateResult, setEndDateResult] = useState('')
+const [locationResult, setLocationResult] = useState('')
+const [companyNameResult, setCompanyNameResult] = useState('')
+const [positionTitleResult, setPositionTitleResult] = useState('')
+const [expStartDateResult, setExpStartDateResult] = useState('')
+const [expEndDateResult, setExpEndDateResult] = useState('')
+const [expLocationResult, setExpLocationResult] = useState('')
+const [fullNameResult, setFullNameResult] = useState('')
+const [emailResult, setEmailResult] = useState('')
+const [phoneResult, setPhoneResult] = useState('')
+const [addressResult, setAddressResult] = useState('')
 
 function resultHandler(e){
-  setResult(e.target.value);
+const name = e.target.name;
+if(name === 'school'){
+setSchoolResult(e.target.value)
+}
+if(name === 'degree'){
+  setDegreeResult(e.target.value)
 }
 
-function eduClick(){
-  setEduForm(<Education resultValue={resultHandler}/>)
+if(name === 'startDate'){
+setStartDateResult(e.target.value)
+}
+if(name === 'endDate'){
+setEndDateResult(e.target.value)
+}
+if(name === 'location'){
+setLocationResult(e.target.value)
+}
+if(name === 'companyName'){
+setCompanyNameResult(e.target.value)
+}
+if(name === 'positionTitle'){
+setPositionTitleResult(e.target.value)
+}
+if(name === 'eduStartDate'){
+setExpStartDateResult(e.target.value)
+}
+if(name === 'eduEndDate'){
+setExpEndDateResult(e.target.value)
+}
+if(name === 'eduLocation'){
+setExpLocationResult(e.target.value)
+}
+if(name === 'description'){
+setDescriptionResult(e.target.value)
+}
+if(name === 'Fullname'){
+setFullNameResult(e.target.value)
+}
+if(name === 'Email'){
+setEmailResult(e.target.value)
+}
+if(name === 'PhoneNumber'){
+setPhoneResult(e.target.value)
+}
+if(name === 'Address'){
+setAddressResult(e.target.value)
 }
 
-function expClick(){
-  setExpForm(<Experience resultValue={resultHandler} />)
 }
 
+
+function eduClick() {
+  const eduKey = generateUniqueKey();
+  setEduForm((prevForms) => [...prevForms, <Education key={eduKey} resultValue={resultHandler} />]);
+}
+
+function expClick() {
+  const expKey = generateUniqueKey();
+  setExpForm((prevForms) => [...prevForms, <Experience key={expKey} resultValue={resultHandler} />]);
+}
+
+function generateUniqueKey() {
+  return Math.random().toString(36).substring(7);
+}
+
+
+
+const [formVisible, setFormVisible] = useState(true)
+
+const toggleFormVisibility = () => {
+  setFormVisible(!formVisible)
+}
 
 
 
@@ -42,22 +118,24 @@ function expClick(){
 
         <div id="detailsContainer">
           <h2>Personal Details</h2>
-          <div id="personalDetailsContainer">
+          <div id="personalDetailsContainer" style={{display: formVisible ? 'block' : 'none' }} >
             <form>
               <label htmlFor="Fullname"><h4>Fullname</h4></label>
-              <input type="text" id='Fullname' name='Fullname'/>
+              <input type="text" id='Fullname'  name='Fullname' onChange={resultHandler}/>
 
               <label htmlFor="Email"><h4>Email</h4></label>
-              <input type="email" id="Email" name='Email' />
+              <input type="email" id="Email" name='Email' onChange={resultHandler} />
 
-              <label htmlFor="Phone Number"><h4>Phone Number</h4></label>
-              <input type="text" id="Phone Number" name='Phone Number'/>
+              <label htmlFor="PhoneNumber"><h4>Phone Number</h4></label>
+              <input type="text" id="PhoneNumber" name='PhoneNumber' onChange={resultHandler}/>
 
               <label htmlFor="Address"><h4>Address</h4></label>
-              <input type="text" id="Address" name="Address"  ></input>
+              <input type="text" id="Address" name="Address" onChange={resultHandler} ></input>
 
             </form>
+            
           </div>
+          <button className='open' onClick={toggleFormVisibility} >Open</button>
 
 
           <div id="detailsButtonsMain">
@@ -83,11 +161,12 @@ function expClick(){
       <div id="cvContainer" className='container'>
         <div id="cvHeader" className='container'>
          
-        <div className="fullNameResult"> <h1>Name Result</h1></div>
+        <div className="fullNameResult"> <h1>{fullNameResult}</h1></div>
+
         <div id="cvHeaderDetails">
-          <div id="emailResult"></div>
-          <div id="phoneResult"></div>
-          <div id="addressResult"></div>
+          <div id="emailResult"><h2>{emailResult}</h2></div>
+          <div id="phoneResult"><h2>{phoneResult}</h2></div>
+          <div id="addressResult"><h2>{addressResult}</h2></div>
         </div>
         </div>
 
@@ -95,14 +174,14 @@ function expClick(){
       <div id="cvEducationHeader" className='container'><h1>Education</h1></div>
       <div id="cvEducationDetailsContainer" className='container'>
         <div id="edDetailsLeft">
-          <div id="degreeResult"><h4>{result}</h4></div>
-          <div id="schoolResult" ><h4>{result}</h4></div>
-          <div id="addressResult"><h4>{result}</h4></div>
+          <div id="degreeResult"><h4>{degreeResult}</h4></div>
+          <div id="schoolResult" name='school' ><h4>{schoolResult}</h4></div>
+          <div id="addressResult"><h4>{locationResult}</h4></div>
         </div>
 
         <div id="edDetailsRight">
-          <div id="eduFromResult"><h4>{result}</h4></div>
-          <div id="eduToResult"><h4>{result}</h4></div>
+          <div id="eduFromResult"><h4>{startDateResult}</h4></div>
+          <div id="eduToResult"><h4>{endDateResult}</h4></div>
         </div>
       </div>
     </div>
@@ -112,20 +191,20 @@ function expClick(){
       <div id="cvExperienceHeader" className='container'><h1>Experience</h1></div>
       <div id="cvExperienceDetialsContainer" className='container'>
         <div id="expDetailsLeft">
-          <div id="companyResult"><h4>{result}</h4></div>
-          <div id="jobResult"><h4>{result}</h4></div>
-          <div id="locationResult"><h4>{result}</h4></div>
+          <div id="companyResult"><h4>{companyNameResult}</h4></div>
+          <div id="jobResult"><h4>{positionTitleResult}</h4></div>
+          <div id="locationResult"><h4>{expLocationResult}</h4></div>
         </div>
 
         <div id="expDetailsRight">
-          <div id="expFromResult"><h4>{result}</h4></div>
-          <div id="expToResult"><h4>{result}</h4></div>
+          <div id="expFromResult"><h4>{expStartDateResult}</h4></div>
+          <div id="expToResult"><h4>{expEndDateResult}</h4></div>
         </div>
       </div>
   
     </div>
     <div id="cvDescriptionContainer" className='container'><h1>Description</h1></div>
-    <h4>{result}</h4>
+    <h4>{descriptionResult}</h4>
       </div>
 
       </div>
--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -6,15 +6,15 @@ export const Education = ({resultValue}) => {
         <>
          <form>
               <label htmlFor="school" ><h4>School</h4></label>
-              <input type="text" onChange={resultValue} name='school' />
+              <input type="text" onChange={resultValue} name='school' id="school" />
               <label htmlFor="degree"><h4>Degree</h4></label>
-              <input type="text" onChange={resultValue} name='degree' />
+              <input type="text" onChange={resultValue} name='degree' id="degree"  />
               <label htmlFor="startDate" ><h4>Start Date</h4></label>
-              <input type="date" onChange={resultValue} name='startDate'/>
+              <input type="date" onChange={resultValue} name='startDate' id="startDate"/>
               <label htmlFor="endDate" ><h4>End Date</h4></label>
-              <input type="date" onChange={resultValue} name='endDate' />
+              <input type="date" onChange={resultValue} name='endDate' id="endDate" />
               <label htmlFor="location" ><h4>Location</h4></label>
-              <input type="text" onChange={resultValue} name='location'/>
+              <input type="text" onChange={resultValue} name='location' id="location" />
               <button className='deleteButton' >Delete</button>
             </form>
         </>)}
@@ -27,17 +27,17 @@ export const Experience = ({resultValue}) => {
             <>
             <form>
             <label htmlFor="companyName"  ><h4>Company Name</h4></label>
-            <input type="text"  onChange={resultValue} name='companyName'/>
-            <label htmlFor="positionTitle"  ><h4>Position Title</h4></label>
-            <input type="text"onChange={resultValue} name='positionTitle'/>
+            <input type="text" onChange={resultValue} name='companyName' id="compayName"/>
+            <label htmlFor="positionTitle" ><h4>Position Title</h4></label>
+            <input type="text" onChange={resultValue} name='positionTitle' id="positionTitle" />
             <label htmlFor="startDate" ><h4>Start Date</h4></label>
-            <input type="date" onChange={resultValue} name='startDate'/>
+            <input type="date" onChange={resultValue} name='expStartDate' id="startDate" />
             <label htmlFor="endDate" ><h4>End Date</h4></label>
-            <input type="date" onChange={resultValue} name='endDate'/>
+            <input type="date" onChange={resultValue} name='expEndDate' id="endDate" />
             <label htmlFor="location" ><h4>Location</h4></label>
-            <input type="text" onChange={resultValue} name='location'/>
+            <input type="text" onChange={resultValue} name='expLocation' id="location" />
             <label htmlFor="description"><h4>Description</h4></label>
-            <input type="text" onChange={resultValue} name='description'/>
+            <input type="text" onChange={resultValue} name='description' id="description" />
             <button className='deleteButton'>Delete</button>
            </form>
            </>
